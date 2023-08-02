e9f92c7e2cd7dc61931a49cf4a6e1e41cc3f5e4c
kyawzinmyat21 <kyawzinmyat@global-connect.asia>
2023-08-02 11:53:04 +0630

    Fixed mobile view error

--- src/components/Goals.jsx
+++ src/components/Goals.jsx
@@ -35,20 +35,20 @@ export default function Goals() {
     return (
         <div className=' w-8/12 mx-auto box my-5 secondary-color p-[3em]'>
             <h1 className='font-extrabold black-color text-3xl text-center p-5 my-[3em]'>Personal Goals and Road Map</h1>
-            <div className='grid grid-rows-5 grid-cols-2 md:grid-cols-10 gap-[5em] p-5 secondary-color font-extrabold'>
-                <div className='col-start-2  mx-auto md:w-full bg-[#ec5656d3] md:col-start-5 md:col-span-4 row-start-1 rounded-lg first'>
+            <div className='grid grid-rows-5 md:grid-cols-10 gap-[5em] p-5 secondary-color font-extrabold'>
+                <div className='mx-auto md:w-full bg-[#ec5656d3] md:col-start-5 md:col-span-4 row-start-1 rounded-lg first'>
                     <p className='p-7 '>start a professional software developer position in the US</p>
                 </div>
-                <div className='col-start-1  row-start-2 md:col-start-3 md:col-span-4  bg-[#df16e2c0] rounded-lg second'>
+                <div className='row-start-2 md:col-start-3 md:col-span-4  bg-[#df16e2c0] rounded-lg second'>
                     <p className='p-7 '>Upgrading my computer science skills by learning design patterns.</p>
                 </div>
-                <div className='col-start-2 row-start-3 md:col-start-4 md:col-span-4  bg-[#52d0aac4] rounded-lg first'>
+                <div className='row-start-3  md:col-start-4 md:col-span-4  bg-[#52d0aac4] rounded-lg first'>
                     <p className='p-7'>Put more emphasis on data structures and algorithms as a major step toward my dream.</p>
                 </div>
                 <div className='row-start-4  bg-[#ddca1ee4]  md:col-start-3 md:col-span-4 rounded-lg first'>
                     <p className=' p-7'>Improving English Language Proficiency for Better Communication</p>
                 </div>
-                <div className='row-start-5 col-start-2  bg-[#c76c57ec] md:col-start-2 md:col-span-4 rounded-lg third'>
+                <div className='row-start-5  bg-[#c76c57ec] md:col-start-2 md:col-span-4 rounded-lg third'>
                     <p className='p-7 '>Pursuing a bachelor's While taking up the experience as much as I can</p>
                 </div>
             </div>
