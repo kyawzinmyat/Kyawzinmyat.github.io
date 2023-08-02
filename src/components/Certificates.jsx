f3e37c393fe45134105b703606ba8fb733b1f6c5
kyawzinmyat21 <kyawzinmyat@global-connect.asia>
2023-08-02 13:07:02 +0630

    Adjust Mobile View

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -3,14 +3,14 @@ import CS50Image from '../assets/cs50.webp'
 import JavaImage from '../assets/java_oo.png'
 export default function Certificates() {
   return (
-    <div className='black-color text-3xl text-center font-bold m-5 p-[5em] box'>
-        <h1>Certificates</h1>
-        <div className='flex flex-wrap md:w-8/12 mx-auto gap-10 my-5'>
-            <div className='shadow-lg md:w-1/3'>
+    <div className='black-color text-3xl text-center font-bold m-5 md:p-[5em] p-[2em] box'>
+        <h1 className='p-[2em]'>Certificates</h1>
+        <div className='flex flex-wrap md:w-8/12 mx-auto gap-y-10 my-5'>
+            <div className='shadow-lg md:w-1/3 w-full'>
                 <img src={CS50Image} alt='CS 50 Image'></img>
                 <p className='p-4 md:text-md text-sm'>Introduction to Computer Science is a course that focuses on developing students' problem-solving abilities rather than their knowledge of various programming syntaxes.</p>
             </div>
-            <div className='shadow-lg md:w-1/3'>
+            <div className='shadow-lg md:w-1/3 w-full'>
                 <img src={JavaImage} alt='Java Object Oriented Image'></img>
                 <p className='p-4 md:text-md text-sm'>This course focused on using the Java programming language for powerful, complex systems and object-oriented programming.</p>
             </div>
