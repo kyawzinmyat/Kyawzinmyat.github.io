dd1193b0ae3f3e0ec244089a656d339d63615dc3
kyawzinmyat21 <kyawzinmyat@global-connect.asia>
2023-08-12 18:11:04 +0630

    drawing canvas

--- src/projects/NeuralNetworkProject/Canvas.jsx
+++ src/projects/NeuralNetworkProject/Canvas.jsx
@@ -1,41 +1,86 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Cell from './Cell';
+import { io } from 'socket.io-client';
 
 export default function Canvas() {
   const g = []
 
   let [isDraw, setIsDraw] = useState(false);
 
-  for (var i = 0; i < 28; i++){
+  for (var i = 0; i < 28; i++) {
     g.push([])
-    for (var j = 0; j < 28; j++){
-        g[i].push(<Cell row={i} col={j}></Cell>)
+    for (var j = 0; j < 28; j++) {
+      g[i].push(<Cell row={i} col={j}></Cell>)
     }
   }
-  let [grids, setGrids] = useState(g)
+  const _socket = io("http://127.0.0.1:5000/", {
+    transports: ["websocket"],
+    cors: {
+      origin: "http://localhost:5173/",
+    },
+  });
+  _socket.on('connect', data => {
+  })
+  let [socket, setSocket] = useState(_socket);
+  let [grids, setGrids] = useState(g);
 
-  const onClickCell = (e) => {
+  // const handleOnclickCell = (row, col) => {
+  //   setGrids(
+  //     grids.map(
+  //       grid => {
+  //         return grid.map(
+  //           gr => {
+  //             if (gr.row === row + 1 || gr.row === row - 1 || gr.col === col + 1 || gr.col === col - 1) {
+  //               return { ...gr, included: true }
+  //             }
+  //             else return gr
+  //           }
+  //         )
+  //       }
+  //     )
+  //   )
+  // }
 
+  useEffect(() => {
+    console.log(socket)
+  }, [socket])
+
+  const handleMouseDownCell = (e) => {
+    setIsDraw(!isDraw)
   }
 
-  const onClickCanvas = () => {
+  const handleOnMouseDownCanvas = (e) => {
     setIsDraw(!isDraw);
   }
 
+  const handleOnMouseMoveCanvas = () => {
+
+  }
+
+  const handleOnMouseUpCanvas = () => {
+
+  }
+
+  var comp;
+
   return (
-    <div className='w-[560px]' onClick={onClickCanvas}>
-        <div className='flex flex-wrap'>
-            {grids.map((grid, row) => {
-                return grid.map((g, col) => {
-                    const cell = React.cloneElement(g, {isDraw: isDraw})
-                    return (
-                        <div key={row + '/' + col}>
-                            {cell}
-                        </div>
-                    )
-                })
-            })}
-        </div>
+    <div ref={el => {
+      // el can be null - see https://reactjs.org/docs/refs-and-the-dom.html#caveats-with-callback-refs
+      if (!el) return;
+      comp = el.getBoundingClientRect();;
+    }} className='w-[280px]' onMouseDown={handleOnMouseDownCanvas} onMouseMove={handleOnMouseMoveCanvas} onMouseUp={handleOnMouseUpCanvas}>
+      <div className='flex flex-wrap'>
+        {grids.map((grid, row) => {
+          return grid.map((g, col) => {
+            const cell = React.cloneElement(g, { row_: row, col_: col, isDraw: isDraw, socket: _socket })
+            return (
+              <div key={row + '/' + col}>
+                {cell}
+              </div>
+            )
+          })
+        })}
+      </div>
     </div>
   )
 }
--- src/projects/NeuralNetworkProject/Cell.jsx
+++ src/projects/NeuralNetworkProject/Cell.jsx
@@ -1,20 +1,41 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { io } from 'socket.io-client';
 
-export default function Cell({row_, col_, isDraw}) {
+
+
+
+export default function Cell({ socket, row_, col_, isDraw, handleOnclickCell, included }) {
 
   let [row, setRow] = useState(row_);
   let [col, setCol] = useState(col_);
   let [clicked, setClicked] = useState(false)
+  let [sent, isSent] = useState(false)
+  let [active, setActive] = useState(false)
+  let [socketObj, setSocketObj] = useState(socket);
 
-  const onClickCell = (e) => {
-    setClicked(!clicked)
-  }
+  // useEffect(() => {
+  //   if (active) {
+  //     socketObj.on('data', data => {
+  //       console.log(data)
+  //     })
+  //   }
+  // }, [socketObj, active])
 
-  const handleMouseMove = (e) => {
-    if (isDraw && !clicked) setClicked(!clicked)
-  }
+  // const onClickCell = (e) => {
+  //   if (!active)     setActive(!active)
+  //   setClicked(!clicked)
+  //   if (!sent) socketObj.emit('data', { row: row, col: col })
+  //   handleOnclickCell(row, col)
+  // }
+
+  // const handleMouseMove = (e) => {
+  //   if (isDraw && !clicked) {
+  //     setClicked(!clicked)
+  //     if (!sent) socketObj.emit('data', { row: row, col: col })
+  //   }
+  // }
 
   return (
-    <p className={'w-[20px] h-[20px] ' + (clicked ? 'primary-bg': 'secondary-bg')} onClick={onClickCell} onMouseMove={handleMouseMove}></p>
+    <p className={'w-[10px] h-[10px] ' + (clicked || included ? 'primary-bg' : 'secondary-bg')}></p>
   )
 }
