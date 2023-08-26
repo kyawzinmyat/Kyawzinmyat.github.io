0aaf6aa661160269bb5a963decbf72b3c9396eb3
kyawzinmyat <kyawzinmyat00000001@gmail.com>
2023-08-26 21:20:54 +0630

    Improve Landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -5,14 +5,14 @@ import { FaGithub, FaFacebook, FaLinkedin } from 'react-icons/fa'
 export default function Landing() {
 
   return (
-    <div className='box wrapper'>
-      <div className='landing-body flex justify-center items-center'>
+    <div className='box landing-body wrapper'>
+      <div className='flex justify-center items-center'>
         <div className='lg:grid lg:grid-cols-1 md:w-8/12 w-10/12 mx-auto my-[4em] grid'>
           {/* <div className='landing-animation'>
             <img src={SteveImage} alt='Steve Image'></img>
           </div> */}
           <div className='primary-color p-10 text-center flex justify-end items-center'>
-            <div className='md:py-[2em] py-[8em] text-pop-animation'>
+            <div className='md:py-[2em] py-[8em] text-pop-animation landing-text'>
               <h1 className='font-bold text-2xl'>Who Am I?</h1>
               <p className='my-5 body-text'>I'm an <strong className=''>enthusiastic software developer</strong> who lives in Yangon.</p>
               <p className='text-md font-light'>Passionate about creating cutting-edge web applications with both frontend and backend functionality.</p>
@@ -25,7 +25,7 @@ export default function Landing() {
           </div>
         </div>
         <div className='foreground'>
-
+          <img src='../assets/landing3.png'></img>
         </div>
       </div>
     </div>
