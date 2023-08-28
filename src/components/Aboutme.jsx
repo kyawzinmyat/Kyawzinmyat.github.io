ab941151b01618301d5dd9fdaa8ff5d1c5ebf0c5
kyawzinmyat <kyawzinmyat00000001@gmail.com>
2023-08-28 23:18:05 +0630

    Enhanced ui

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -15,9 +15,15 @@ export default function Aboutme() {
                 transform: isInView ? 'none' : 'translateY(2000px)', opacity: isInView ? 1 : 0,
                 transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
             }}>
-                <div className='grid grid-cols-3 md:gap-10 gap-y-10'>
-                    <motion.div className='primary-color rounded-[5px]  shadow-lg col-span-2'>
-
+                <div className='grid grid-cols-1 md:grid-cols-3 md:gap-10 gap-y-10'>
+                    <motion.div className='flex md:col-span-3 p-[2em] justify-end'>
+                        <ul className='flex gap-5 md:text-[1rem] text-[0.8rem] p-[0.1em] about-me-sections'>
+                            <li className='p-[1em] border-2 border-white secondary-bg rounded-full' style={active==='professional'?{backgroundColor: 'var(--secondary-accent-color)', color: 'black'} : {}} onClick={() => setActive('professional')}>Professional</li>
+                            <li className='p-[1em] border-2 border-white secondary-bg rounded-full' style={active==='personal'?{backgroundColor: 'var(--secondary-accent-color)', color: 'black'} : {}} onClick={() => setActive('personal')}>Personal</li>
+                            <li className='p-[1em] border-2 border-white secondary-bg rounded-full' style={active==='education'?{backgroundColor: 'var(--secondary-accent-color)', color: 'black'} : {}} onClick={() => setActive('education')}>Education</li>
+                        </ul>
+                    </motion.div>
+                    {active === 'professional' &&  <motion.div className='primary-color rounded-[5px]  shadow-lg col-span-2' initial={{height: 0, overflow: 'hidden'}} whileInView={{height: '100%', transition: 'height 3s cubic-bezier(0,1.5,1,1.5)'}}>
                         <div className='p-[2em] w-full lg:w-10/12 text-left flex justify-center items-center'>
                             <div>
                                 <motion.h1 initial="hidden"
@@ -33,15 +39,46 @@ export default function Aboutme() {
                                 </div>
                             </div>
                         </div>
-                    </motion.div>
+                    </motion.div>}
 
-                    <motion.div className='flex md:justify-end md:items-top'>
-                        <ul className='md:flex xl:flex-row  gap-5 flex-col md:text-[1rem] text-[0.5rem] p-[0.1em] about-me-sections'>
-                            <li className='p-[1em] border-b-2'>Professional</li>
-                            <li className='p-[1em] border-b-2'>Personal</li>
-                            <li className='p-[1em] border-b-2'>Educational</li>
-                        </ul>
-                    </motion.div>
+                    {active === 'personal' && <motion.div className='primary-color rounded-[5px]  shadow-lg col-span-2' initial={{height: 0, overflow: 'hidden'}} whileInView={{height: '100%', transition: 'height 3s cubic-bezier(0,1.5,1,1.5)'}}>
+                        <div className='p-[2em] w-full lg:w-10/12 text-left flex justify-center items-center'>
+                            <div>
+                                <motion.h1 initial="hidden"
+                                    animate='visible'
+                                    transition={{ duration: 10 }}
+                                    className='text-lg font-bold md:text-2xl my-3'>
+                                    Personal
+                                </motion.h1>
+                                <hr></hr>
+                                <div className='font-md text-sm md:text-md lg:py[5em] py-[3em]'>
+                                <p>I'm currently 20 years old living in Kamayut Township, Yangon. Playing video games and learning about new technologies, such as networking, software, and computer architecture, are two of my passions.</p>
+                                    <p className='my-3'> I'm pursuing a Bachelor in Computer Science at an online university right now.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </motion.div>}
+
+                    {active === 'education' && <motion.div className='primary-color rounded-[5px]  shadow-lg col-span-2' initial={{height: 0, overflow: 'hidden'}} whileInView={{height: '100%', transition: 'height 3s cubic-bezier(0,1.5,1,1.5)'}}>
+                        <div className='p-[2em] w-full lg:w-10/12 text-left flex justify-center items-center'>
+                            <div>
+                                <motion.h1 initial="hidden"
+                                    animate='visible'
+                                    transition={{ duration: 10 }}
+                                    className='text-lg font-bold md:text-2xl my-3'>
+                                    Education
+                                </motion.h1>
+                                <hr></hr>
+                                <div className='font-md text-sm md:text-md lg:py[5em] py-[3em]'>
+                                <p>In 2019, I earned my high school diploma.
+                                        At UCSY, I majored in computer science, but I no longer do so due to the political controversy.</p>
+                                    <p className='my-3'>During the COVID pandemic, I took computer science courses like CS50 and Object-Oriented Java programming to advance my skills.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </motion.div>}
+
+
 
                     {/* <motion.div className='grid md:grid-cols-3 grid-cols-1 secondary-bg primary-color rounded-[5px] shadow-lg'>
                         <img src={PersonalImage} alt='steve professioanl image' className='md:col-span-1'></img>
--- src/components/TechnicalStacks.jsx
+++ src/components/TechnicalStacks.jsx
@@ -69,7 +69,7 @@ export default function TechnicalStacks() {
             <div className='primary-color'>
                 <h1 className='font-extrabold text-center primary-color header-upper-text'>Technical Stack</h1>
                 <div className='md:w-4/12 w-8/12 my-5 mx-auto'>
-                    <p className='text-center md:p-4 md:text-md font-md text-sm'>I used a variety of technologies as a Developer responsible for building dynamic web apps to provide high-quality software. </p>
+                    <p className='text-center md:p-4 my-[3rem] md:text-md font-md text-sm'>I used a variety of technologies as a Developer responsible for building dynamic web apps to provide high-quality software. </p>
                 </div>
             </div>
             <div className='primary-color my-4'>
@@ -83,7 +83,7 @@ export default function TechnicalStacks() {
             </div>
             <div className='grid md:grid-cols-3 mx-auto progress section-margin'>
                 {show && <>
-                    <div className='secondary-bg p-5 m-3'>
+                    <div className='secondary-bg md:p-5 md:m-3'>
                         <h1 className='primary-color text-sm lg:text-3xl text-center p-5' >Programming Languages</h1>
                         <div className='py-[3em] px-5 flex flex-col gap-6'>
                             <div className='gap-5 primary-color font-extrabold grid grid-cols-5'>
@@ -109,7 +109,7 @@ export default function TechnicalStacks() {
                             </div>
                         </div>
                     </div>
-                    <div className='secondary-bg p-5 m-3'>
+                    <div className='secondary-bg md:p-5 md:m-3'>
                         <h1 className='primary-color text-sm lg:text-3xl text-center p-5'>Frameworks</h1>
                         <div className='py-[3em] px-5 flex flex-col gap-6'>
                             <div className='gap-5 primary-color font-extrabold grid grid-cols-5'>
@@ -157,7 +157,7 @@ export default function TechnicalStacks() {
                         </div>
 
                     </div>
-                    <div className='secondary-bg p-5 m-3'>
+                    <div className='secondary-bg md:p-5 md:m-3'>
                         <h1 className='primary-color text-sm lg:text-3xl text-center p-5'>Markup Languages</h1>
                         <div className='py-[3em] px-5 flex flex-col gap-6'>
                             <div className='gap-5 primary-color font-extrabold grid grid-cols-5'>
